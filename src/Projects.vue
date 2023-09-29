c51bcf239677d6111bbe3a49378b87c696d91bc7
grow-xd <gupta.rg.rishabh@gmail.com>
2023-09-29 22:57:37 +0530

    Testing

---
+++ src/Projects.vue
@@ -0,0 +1,176 @@
+<template>
+    <div class="bg-gradient-to-r from-[#072e33] to-[#05161a] duration-800 p-4">
+            <!-- Your design content for the third state -->
+            <!-- component -->
+                <div class="sticky top-0 h-screen flex flex-col items-center justify-center bg-gradient-to-r from-[#16272B] to-[#183f44] ">
+
+                    <div
+                        class="container mx-auto grid grid-cols-1 p-4 pl-10 md:grid-cols-2 items-center md:gap-20 bg-gradient-to-t from-[#fffbf5] to-[#fffef4]">
+                        <div class="content">
+                            <div class="flex items-center gap-3">
+                                <hr class="w-10 bg-orange-500 border " />
+                                <span class="md:text-[18px] font-medium text-gray-800 ">
+                                    Latest Frontend Project
+                                </span>
+                            </div>
+                            <p class="text-[40px] lg:text-[45px] xl:text-[55px] font-bold leading-tight mt-5 sm:mt-0 ">
+                                Personal <br />
+                                Portfolio Website
+                            </p>
+                            <p class="mt-5 md:text-md ">
+                                Its one of my Frontend project which is built in Tailwind CSS 
+                                and Vue Js. <br class="hidden md:block" /> It showcases Technical and Academic Information about myself.
+                            </p>
+                            <div class="flex gap-4 mt-10">
+
+                                <button
+                                    class="font-medium text-[16px] flex items-center px-5 py-3 md:py-4 md:px-8 rounded-xl capitalize bg-gradient-to-r from-orange-300 to-orange-500 hover:from-pink-500 hover:to-yellow-500  relative gap-2 transition duration-300 hover:scale-105 text-white shadow-glass ">
+                                    Check Project
+                                </button>
+
+                                <a href="https://github.com/grow-xd/Portfolio"
+                                    class="font-medium text-[16px] flex items-center px-5 py-3 md:py-4 md:px-8 rounded-xl capitalize bg-gradient-to-r from-orange-300 to-orange-500 hover:from-pink-500 hover:to-yellow-500  relative gap-2 transition duration-300 hover:scale-105 text-white shadow-glass ">
+                                    Github Repo
+                            </a>
+                                <RiFacebookFill class="" />
+                                <SiBehance />
+                            </div>
+
+                        </div>
+                        <div class="relative sm:mt-0 mt-10 px-6 sm:px-0">
+
+                            <img class="w-[600px] animate__animated animate__fadeInRight animate__delay-.5s"
+                                src="https://images.unsplash.com/photo-1624628639856-100bf817fd35?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8M2QlMjBpbWFnZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60"
+                                alt="" />
+                        </div>
+                    </div>
+                </div>
+                <div class="sticky top-8 h-screen flex flex-col items-center justify-center bg-gradient-to-r from-[#27383c] to-[#294f55] text-white ">
+                    <div
+                        class="container mx-auto grid grid-cols-1 p-4 pl-10 md:grid-cols-2 items-center md:gap-20 bg-gradient-to-t from-[#fffbf5] to-[#fffef4]">
+                        <div class="content">
+                            <div class="flex items-center gap-3">
+                                <hr class="w-10 bg-orange-500 border " />
+                                <span class="md:text-[18px] font-medium text-gray-800 ">
+                                    Latest Backend Project
+                                </span>
+                            </div>
+                            <p class="text-[40px] lg:text-[45px] xl:text-[55px] text-black font-bold leading-tight mt-5 sm:mt-0 ">
+                                Project <br />
+                                Gallery REST APIs
+                            </p>
+                            <p class="mt-5 md:text-md text-black ">
+                                Its one of my Backend project which is built in Django-Rest-Framework
+                                 <br class="hidden md:block" /> It was built for the SIH.
+                            </p>
+                            <div class="flex gap-4 mt-10">
+
+                                <button
+                                    class="font-medium text-[16px] flex items-center px-5 py-3 md:py-4 md:px-8 rounded-xl capitalize bg-gradient-to-r from-orange-300 to-orange-500 hover:from-pink-500 hover:to-yellow-500  relative gap-2 transition duration-300 hover:scale-105 text-white shadow-glass ">
+                                    Check Project
+                                </button>
+
+                                <a href="https://github.com/grow-xd/Project-engine"
+                                    class="font-medium text-[16px] flex items-center px-5 py-3 md:py-4 md:px-8 rounded-xl capitalize bg-gradient-to-r from-orange-300 to-orange-500 hover:from-pink-500 hover:to-yellow-500  relative gap-2 transition duration-300 hover:scale-105 text-white shadow-glass ">
+                                    Github Repo
+                            </a>
+                                <RiFacebookFill class="" />
+                                <SiBehance />
+                            </div>
+
+                        </div>
+                        <div class="relative sm:mt-0 mt-10 px-6 sm:px-0">
+
+                            <img class="w-[600px] animate__animated animate__fadeInRight animate__delay-.5s"
+                                src="https://images.unsplash.com/photo-1624628639856-100bf817fd35?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8M2QlMjBpbWFnZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60"
+                                alt="" />
+                        </div>
+                    </div>
+                </div>
+                <div class="sticky top-16 h-screen flex flex-col items-center justify-center bg-gradient-to-r from-[#16272B] to-[#183f44] text-white ">
+                    <div
+                        class="container mx-auto grid grid-cols-1 p-4 pl-10 md:grid-cols-2 items-center md:gap-20 bg-gradient-to-t from-[#fffbf5] to-[#fffef4]">
+                        <div class="content">
+                            <div class="flex items-center gap-3">
+                                <hr class="w-10 bg-orange-500 border " />
+                                <span class="md:text-[18px] font-medium text-gray-800 ">
+                                    Not updated....
+                                </span>
+                            </div>
+                            <p class="text-[40px] lg:text-[45px] xl:text-[55px] font-bold leading-tight mt-5 sm:mt-0 ">
+                                Personal <br />
+                                Portfolio Website
+                            </p>
+                            <p class="mt-5 md:text-md ">
+                                Its one of my Frontend project which is built in Tailwind CSS 
+                                and Vue Js. <br class="hidden md:block" /> It showcases Technical and Academic Information about myself.
+                            </p>
+                            <div class="flex gap-4 mt-10">
+
+                                <button
+                                    class="font-medium text-[16px] flex items-center px-5 py-3 md:py-4 md:px-8 rounded-xl capitalize bg-gradient-to-r from-orange-300 to-orange-500 hover:from-pink-500 hover:to-yellow-500  relative gap-2 transition duration-300 hover:scale-105 text-white shadow-glass ">
+                                    Check Project
+                                </button>
+
+                                <button
+                                    class="font-medium text-[16px] flex items-center px-5 py-3 md:py-4 md:px-8 rounded-xl capitalize bg-gradient-to-r from-orange-300 to-orange-500 hover:from-pink-500 hover:to-yellow-500  relative gap-2 transition duration-300 hover:scale-105 text-white shadow-glass ">
+                                    Github Repo
+                                </button>
+                                <RiFacebookFill class="" />
+                                <SiBehance />
+                            </div>
+
+                        </div>
+                        <div class="relative sm:mt-0 mt-10 px-6 sm:px-0">
+
+                            <img class="w-[600px] animate__animated animate__fadeInRight animate__delay-.5s"
+                                src="https://images.unsplash.com/photo-1624628639856-100bf817fd35?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8M2QlMjBpbWFnZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60"
+                                alt="" />
+                        </div>
+                    </div>
+                </div>
+                <div class="sticky top-24 h-screen flex flex-col items-center justify-center bg-gradient-to-r from-[#27383c] to-[#294f55] text-white ">
+                    <div
+                        class="container mx-auto grid grid-cols-1 p-4 pl-10 md:grid-cols-2 items-center md:gap-20 bg-gradient-to-t from-[#fffbf5] to-[#fffef4]">
+                        <div class="content">
+                            <div class="flex items-center gap-3">
+                                <hr class="w-10 bg-orange-500 border " />
+                                <span class="md:text-[18px] font-medium text-gray-800 ">
+                                    Not Updated....
+                                </span>
+                            </div>
+                            <p class="text-[40px] lg:text-[45px] xl:text-[55px] font-bold leading-tight mt-5 sm:mt-0 ">
+                                Personal <br />
+                                Portfolio Website
+                            </p>
+                            <p class="mt-5 md:text-md ">
+                                Its one of my Frontend project which is built in Tailwind CSS 
+                                and Vue Js. <br class="hidden md:block" /> It showcases Technical and Academic Information about myself.
+                            </p>
+                            <div class="flex gap-4 mt-10">
+
+                                <button
+                                    class="font-medium text-[16px] flex items-center px-5 py-3 md:py-4 md:px-8 rounded-xl capitalize bg-gradient-to-r from-orange-300 to-orange-500 hover:from-pink-500 hover:to-yellow-500  relative gap-2 transition duration-300 hover:scale-105 text-white shadow-glass ">
+                                    Check Project
+                                </button>
+
+                                <button
+                                    class="font-medium text-[16px] flex items-center px-5 py-3 md:py-4 md:px-8 rounded-xl capitalize bg-gradient-to-r from-orange-300 to-orange-500 hover:from-pink-500 hover:to-yellow-500  relative gap-2 transition duration-300 hover:scale-105 text-white shadow-glass ">
+                                    Github Repo
+                                </button>
+                                <RiFacebookFill class="" />
+                                <SiBehance />
+                            </div>
+
+                        </div>
+                        <div class="relative sm:mt-0 mt-10 px-6 sm:px-0">
+
+                            <img class="w-[600px] animate__animated animate__fadeInRight animate__delay-.5s"
+                                src="https://images.unsplash.com/photo-1624628639856-100bf817fd35?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8M2QlMjBpbWFnZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60"
+                                alt="" />
+                        </div>
+                    </div>
+                </div>
+            
+        </div>
+</template>
---
+++ src/introduction.vue
@@ -0,0 +1,169 @@
+<template>
+    <div class="bg-gradient-to-r from-slate-900 to-slate-700 bg-auto duration-800 p-4">
+
+<div class="flex flex-col items-center justify-center h-3/6 pb-20 px-8">
+    <div class="flex flex-col items-center justify-center text-center pt-20 h-screen-half">
+
+        <h1 class="text-5xl text-white font-nothing sm:text-6xl lg:text-9xl">
+            <span
+                class="bg-gradient-to-r from-yellow-200 to-violet-200 bg-no-repeat [background-position:0_88%] [background-size:100%_0.05em] motion-safe:transition-all motion-safe:duration-200 hover:[background-size:100%_100%] hover:text-gray-700 focus:[background-size:100%_100%] rounded-full">
+                Rishabh Gupta
+            </span>
+        </h1>
+    </div>
+</div>
+
+<div class=" flex flex-cols grid lg:grid-cols-2 grid-cols-1 gap-4">
+
+    <!-- Border animation Here -->
+    <div class="items-center grid justify-items-center content-center">
+        <div
+            class="relative w-3/5 h-w border before:absolute before:-bottom-2 before:-right-2 before:h-4 before:w-4 before:border-b before:border-r before:transition-all before:duration-300 before:ease-in-out after:absolute after:-top-2 after:-left-2 after:h-4 after:w-4 after:border-t after:border-l after:transition-all after:duration-300 after:ease-in-out hover:before:h-[calc(100%+16px)] hover:before:w-[calc(100%+16px)] hover:after:h-[calc(100%+16px)] hover:after:w-[calc(100%+16px)]">
+            <img class="w-full" src="../public/images/profile.jpg" alt="Your Photo" />
+
+        </div>
+    </div>
+
+
+    <!-- Right column for your information -->
+    <div class="w-screen-half justify-between text-center items-center pr-6">
+        <span class=" link link-underline link-underline-black text-2xl text-white font-nothing">
+            About Me
+        </span>
+        <hr
+            class="my-12 h-px border-t-0 bg-transparent bg-gradient-to-r from-transparent via-neutral-500 to-transparent opacity-25 dark:opacity-100" />
+        <span class="text -xl text-white font-nothing">
+            I am a Software Developer Student with 2 years of experience in developing python projects as local
+            freelancing. Currently experienced in developing web applications in django and vue.
+            I have completed many web development projects in my past.
+            I always try to find a better way of doing the existing thing and have a thrust to try and challenge
+            myself with new opportunities and technologies.
+        </span>
+        <div class="w-full m-4 pb-20">
+            <div
+                class="coding inverse-toggle px-5 pt-4 shadow-lg text-gray-100 text-sm font-mono subpixel-antialiased bg-gray-800  pb-6 pt-4 rounded-lg leading-normal overflow-hidden space-y-4">
+                <div class="top mb-2 flex">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        class="w-6 h-6 stroke-green-400">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M17.25 6.75L22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3l-4.5 16.5" />
+                    </svg>
+
+
+                    <span class="text-green-400 space-x-4 text-sm font-bold">... Languages: </span>
+                </div>
+                <div class="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mt-4 space-y-2">
+
+                    <p class="flex-1 typing pl-2">
+                        Python
+                        <br>
+                    </p>
+                    <p class="flex-1 typing pl-2">
+                        C/C++
+                        <br>
+                    </p>
+                    <p class="flex-1 typing pl-2">
+                        Javascript
+                        <br>
+                    </p>
+                    <p class="flex-1 typing pl-2">
+                        SQL
+                        <br>
+                    </p>
+                    <p class="flex-1 typing pl-2">
+                        HTML
+                        <br>
+                    </p>
+                    <p class="flex-1 typing pl-2">
+                        CSS
+                        <br>
+                    </p>
+
+                </div>
+                <div class="top mb-2 flex">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        class="w-6 h-6 stroke-green-400">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M17.25 6.75L22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3l-4.5 16.5" />
+                    </svg>
+
+
+                    <span class="text-green-400 space-x-4 text-sm font-bold">... Technologies: </span>
+                </div>
+                <div class="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mt-4 space-y-2">
+
+                    <p class="flex-1 typing pl-2">
+                        Django
+                        <br>
+                    </p>
+                    <p class="flex-1 typing pl-2">
+                        Django-Rest-Framework
+                        <br>
+                    </p>
+                    <p class="flex-1 typing pl-2">
+                        Tailwind CSS
+                        <br>
+                    </p>
+                    <p class="flex-1 typing pl-2">
+                        Vue Js
+                        <br>
+                    </p>
+                    <p class="flex-1 typing pl-2">
+                        MySQL
+                        <br>
+                    </p>
+                    <p class="flex-1 typing pl-2">
+                        Git & Github
+                        <br>
+                    </p>
+                    <p class="flex-1 typing pl-2">
+                        AWS
+                        <br>
+                    </p>
+                    <p class="flex-1 typing pl-2">
+                        Linux
+                        <br>
+                    </p>
+                    <p class="flex-1 typing pl-2">
+                        Vs Code
+                        <br>
+                    </p>
+
+                </div>
+                <div class="top mb-2 flex">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        class="w-6 h-6 stroke-green-400">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M17.25 6.75L22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3l-4.5 16.5" />
+                    </svg>
+
+
+                    <span class="text-green-400 space-x-4 text-sm font-bold">... Interests: </span>
+                </div>
+                <div class="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mt-4 space-y-2">
+
+                    <p class="flex-1 typing pl-2">
+                        Cyber Security
+                        <br>
+                    </p>
+                    <p class="flex-1 typing pl-2">
+                        Open-Source
+                        <br>
+                    </p>
+                    <p class="flex-1 typing pl-2">
+                        Backend Planning
+                        <br>
+                    </p>
+
+
+                </div>
+            </div>
+        </div>
+
+
+    </div>
+
+</div>
+
+</div>
+</template>
